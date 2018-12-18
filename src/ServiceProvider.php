d81a4b96b1e32016d1c159d232eb2438a8e86cf8
Mark Babič <mark.babic@redhub.com.au>
2018-12-19 10:14:12 +1100

    Service provider and laravel auto register

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,7 +2,34 @@
 
 namespace Bearcodi\DockerSecrets;
 
-class ServiceProvider
-{
+use Bearcodi\DockerSecrets\DockerSecrets;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+    * Perform post-registration booting of services.
+    *
+    * @return void
+    */
+    public function boot()
+    {
+        (new DockerSecrets())->parse(config()->all());
+        // $this->publishes([
+        //     __DIR__.'/path/to/config/courier.php' => config_path('courier.php'),
+        // ]);
+
+    }
+
+    /**
+    * Register bindings in the container.
+    *
+    * @return void
+    */
+    public function register()
+    {
+        // $this->mergeConfigFrom(
+        //     __DIR__.'/path/to/config/courier.php', 'courier'
+        // );
+    }
 }
