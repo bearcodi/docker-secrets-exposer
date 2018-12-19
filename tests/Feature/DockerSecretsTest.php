c23feb409def5504b4555696fad0d131427838b3
Mark Babič <mark.babic@redhub.com.au>
2018-12-19 14:27:08 +1100

    Check Docker secret is not exposed to docker environment

--- tests/Feature/DockerSecretsTest.php
+++ tests/Feature/DockerSecretsTest.php
@@ -13,17 +13,32 @@ class DockerSecretsTest extends TestCase
     public function it_can_expand_a_docker_secret()
     {
         $dockerSecret = $this->dockerSecretFile('pied', 'piper');
-        $envKey = 'DOCKER_SECRET';
+        $configKey = 'docker.secret';
 
-        config(["{$envKey}" => $dockerSecret->dsn()]);
+        config(["{$configKey}" => $dockerSecret->dsn()]);
 
         (new DockerSecrets())->parse(config()->all());
 
-        $secret = config($envKey);
+        $secret = config($configKey);
 
         $this->assertInstanceOf(Secret::class, $secret);
 
         $this->assertEquals('piper', (string) $secret);
     }
 
+    /** @test */
+    public function a_docker_secret_is_not_exposed_to_the_environment()
+    {
+        $envKey = 'DOCKER_SECRET';
+        $configKey = 'docker.secret';
+        $dockerSecret = $this->dockerSecretFile('pied', 'piper');
+
+        putenv("{$envKey}={$dockerSecret->dsn()}");
+        config(["{$configKey}" => $dockerSecret->dsn()]);
+        (new DockerSecrets())->parse(config()->all());
+
+        $this->assertInstanceOf(Secret::class, config($configKey));
+        $this->assertEquals('piper', config($configKey)->expose());
+        $this->assertEquals(getenv($envKey), $dockerSecret->dsn());
+    }
 }
