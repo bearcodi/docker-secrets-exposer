2b62b1fe481fe07762a34f70ed681c5ce0188919
Mark Babič <mark@bearcodi.com>
2019-01-08 09:16:58 +1100

    Cleanup service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -14,20 +14,5 @@ class ServiceProvider extends BaseServiceProvider
     public function boot()
     {
         (new DockerSecrets())->parse(config()->all());
-        // $this->publishes([
-        //     __DIR__.'/path/to/config/courier.php' => config_path('courier.php'),
-        // ]);
-    }
-
-    /**
-     * Register bindings in the container.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        // $this->mergeConfigFrom(
-        //     __DIR__.'/path/to/config/courier.php', 'courier'
-        // );
     }
 }
